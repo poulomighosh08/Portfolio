b1c560148bc9ba470a1add78d048e9a3d0e155c6
ghoshpoulomi08 <ghoshpoulomi1308@gmail.com>
2023-08-26 15:26:39 +0530

    Added env variable support

--- send_email.py
+++ send_email.py
@@ -1,9 +1,10 @@
 import smtplib, ssl
+import os
 def send_user_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "ghosh.poulomisweet@gmail.com"
-    password = "hmrdopkxfwmzvola"
+    password = os.getenv("PASSWORD")
     receiver = "ghosh.poulomisweet@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
